b9c5c7f9dfd4996d807f3036522c927d554f8078
kamaljameel <kamaljameel14@gmail.com>
2023-10-25 00:24:40 +0500

    product detail page 70% complete

--- figma-to-html/js/main.js
+++ figma-to-html/js/main.js
@@ -165,3 +165,123 @@ $(document).ready(function(){
     ]
   })
    });
+
+   $(document).ready(function() {
+    var slider = $("#slider");
+    var thumb = $("#thumb");
+    var slidesPerPage = 4; //globaly define number of elements per page
+    var syncedSecondary = true;
+    slider.owlCarousel({
+        items: 1,
+        slideSpeed: 2000,
+        nav: false,
+        autoplay: false, 
+        dots: false,
+        loop: true,
+        responsiveRefreshRate: 200
+    }).on('changed.owl.carousel', syncPosition);
+    thumb
+        .on('initialized.owl.carousel', function() {
+            thumb.find(".owl-item").eq(0).addClass("current");
+        })
+        .owlCarousel({
+            items: slidesPerPage,
+            dots: false,
+            nav: true,
+            item: 4,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: slidesPerPage, 
+          navText: ['<svg width="18px" height="18px" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="25px" height="25px" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+            responsiveRefreshRate: 100
+        }).on('changed.owl.carousel', syncPosition2);
+    function syncPosition(el) {
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+        if (current < 0) {
+            current = count;
+        }
+        if (current > count) {
+            current = 0;
+        }
+        thumb
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+        var onscreen = thumb.find('.owl-item.active').length - 1;
+        var start = thumb.find('.owl-item.active').first().index();
+        var end = thumb.find('.owl-item.active').last().index();
+        if (current > end) {
+            thumb.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+            thumb.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            slider.data('owl.carousel').to(number, 100, true);
+        }
+    }
+    thumb.on("click", ".owl-item", function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        slider.data('owl.carousel').to(number, 300, true);
+    });
+
+
+        $(".qtyminus").on("click",function(){
+            var now = $(".qty").val();
+            if ($.isNumeric(now)){
+                if (parseInt(now) -1> 0)
+                { now--;}
+                $(".qty").val(now);
+            }
+        })            
+        $(".qtyplus").on("click",function(){
+            var now = $(".qty").val();
+            if ($.isNumeric(now)){
+                $(".qty").val(parseInt(now)+1);
+            }
+        });
+});
+
+const productImages = document.querySelectorAll('.productzoomImg img');
+
+productImages.forEach((image) => {
+  image.addEventListener('mouseover', () => {
+    zoomIn(image);
+  });
+
+  image.addEventListener('mouseout', () => {
+    zoomOut(image);
+  });
+
+  image.addEventListener('mousemove', (e) => {
+    zoomMove(e, image);
+  });
+});
+
+function zoomIn(image) {
+  image.style.transform = 'scale(1.5)';
+}
+
+function zoomOut(image) {
+  image.style.transform = 'scale(1)';
+}
+
+function zoomMove(e, image) {
+  const imageRect = image.getBoundingClientRect();
+  const x = e.clientX - imageRect.left;
+  const y = e.clientY - imageRect.top;
+
+  const scaleX = 1.5;
+  const scaleY = 1.5;
+
+  const transformOriginX = (x / imageRect.width) * 100;
+  const transformOriginY = (y / imageRect.height) * 100;
+
+  image.style.transformOrigin = `${transformOriginX}% ${transformOriginY}%`;
+}
